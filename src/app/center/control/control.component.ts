52296a5fd7d5b0851bad41446a1f17f5df27895f
meng.wang <meng.wang@live.com>
2018-09-27 17:22:10 +0800

    md demo change

--- src/app/center/control/control.component.ts
+++ src/app/center/control/control.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./control.component.css']
 })
 export class ControlComponent implements OnInit {
-  current = 0;
+
   ngOnInit() {
 
   }
