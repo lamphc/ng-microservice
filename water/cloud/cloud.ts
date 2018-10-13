0ae58ff7b2ffbe6edd2a8e17de028751bc645043
meng.wang <meng.wang@live.com>
2018-10-13 10:20:54 +0800

    init base for master dev

--- water/cloud/cloud.ts
+++
@@ -1,5 +0,0 @@
-export default class WaterCloud {
-    constructor(parameters) {
-
-    }
-}
--- water/index.ts
+++
@@ -1,8 +0,0 @@
-import * as Theme from "./theme/theme";
-
-const WaterTheme = new Theme.default();
-
-
-export { TranslateModule, TranslateLoader, TranslateHttpLoader, HttpLoaderFactory, TranslatePipe, TranslateService } from "./lang/lang";
-
-export default WaterTheme;
